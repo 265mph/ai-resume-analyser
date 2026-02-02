9a93924fc29bcee007b71f3a049c5419fa5bfe0d
Aina Temiloluwa Samuel <samuelaina18@gmail.com>
2026-02-02 13:40:43 +0100

    tailwind commands and hero section

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,32 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "constants/index";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Analyser" },
+    { name: "AI Resume Analyser", content: "An AI that works for you, not the Recruiter" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications and Resume Ratings</h1>
+        <h2>Get AI-powered feedback on your Resumes</h2>
+      </div>
+    </section>
+
+  {resumes.length > 0 && (
+    <div className="resumes-section">
+      {resumes.map((resume:Resume) => (
+        <ResumeCard key={resume.id} resume={resume} />
+      ))}
+    </div>
+  )}
+  </main>;
 }
